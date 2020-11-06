18c2d2dae770588de65db67f4d63d6f00f2fda79
jeffersoncuartas <jefferson.cuartas@gmail.com>
2020-11-06 17:20:08 -0500

    testing

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,45 @@
 # Add your code here
+
+class Dog
+
+    attr_accessor :name
+
+    @@all = []
+
+    def initialize(name)
+
+        @name = name
+        self.save 
+
+    end 
+
+    def save #save is an instance method, not a class method, because you're only saving one instance at a time. 
+
+        @@all << self
+
+    end 
+    
+
+    def self.all
+        @@all
+    end
+
+    def self.clear_all
+
+        @@all = []
+
+    end 
+    
+    def self.print_all
+
+        @@all.each do |dog|
+
+            puts dog.name 
+
+        end 
+
+    end 
+
+
+
+end 
